f6ab02a34f1a6175bee82be43f153ef11a7b3e36
mgoodson <darth123>
2018-04-19 15:41:32 -0500

    2018-04-20 MGOODSON1 Unit 12 Particpation Activity

--- app/src/main/java/css/cis3334/firebaseauthentication/MainActivity.java
+++ app/src/main/java/css/cis3334/firebaseauthentication/MainActivity.java
@@ -1,6 +1,6 @@
 package css.cis3334.firebaseauthentication;
 
-import android.content.Intent;
+
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,20 +11,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonGoogleLogin;
     private Button buttonCreateLogin;
     private Button buttonSignOut;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         buttonGoogleLogin = (Button) findViewById(R.id.buttonGoogleLogin);
         buttonCreateLogin = (Button) findViewById(R.id.buttonCreateLogin);
         buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        mAuth = FirebaseAuth.getInstance();
+
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -80,16 +75,75 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void createAccount(String email, String password) {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        if (currentUser != null) {
+            // User is signed in
+            Log.d("CIS33334", "onAuthStateChanged:signed_in:" + currentUser.getUid());
+            Toast.makeText(MainActivity.this, "User Signed In", Toast.LENGTH_LONG).show();
+            textViewStatus.setText("Signed In");
+        } else {
+            // User is signed out
+            Log.d("CIS33334", "onAuthStateChanged:signed_out:");
+            Toast.makeText(MainActivity.this, "User Signed Out", Toast.LENGTH_LONG).show();
+            textViewStatus.setText("Signed Out");
+        }
+    }
 
+    private void createAccount(String email, String password) {
+        Log.d("CIS33334", "CreateAccount:" + email);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CIS3334", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            textViewStatus.setText("Signed In");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CIS3334", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            textViewStatus.setText("Signed Out");
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CIS3334", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            textViewStatus.setText("Signed In");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CIS3334", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            textViewStatus.setText("Signed Out");
+                        }
 
+                        // ...
+                    }
+                });
     }
 
     private void signOut () {
-
+        mAuth.signOut();
+        textViewStatus.setText("Signed Out");
     }
 
     private void googleSignIn() {
